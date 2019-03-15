f3cf599205d3bc35c6e7abb0c888644d32cf126f
amenchu <amaresh_menchu@gap.com>
2019-03-15 21:10:24 +0530

    headers added

--- spring-boot-rabbitmq-publisher/src/main/java/in/spring4buddies/poc/configuration/RabbitMQConfig.java
+++ spring-boot-rabbitmq-publisher/src/main/java/in/spring4buddies/poc/configuration/RabbitMQConfig.java
@@ -1,9 +1,6 @@
 package in.spring4buddies.poc.configuration;
 
-import org.springframework.amqp.core.Binding;
-import org.springframework.amqp.core.BindingBuilder;
-import org.springframework.amqp.core.DirectExchange;
-import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.HeadersExchange;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
@@ -24,18 +21,8 @@ public class RabbitMQConfig {
 	private String routingKey;
 
 	@Bean
-	public Queue queue() {
-		return new Queue(queueName, false);
-	}
-
-	@Bean
-	public DirectExchange exchange() {
-		return new DirectExchange(exchange);
-	}
-
-	@Bean
-	public Binding binding(Queue queue, DirectExchange exchange) {
-		return BindingBuilder.bind(queue).to(exchange).with(routingKey);
+	public HeadersExchange exchange() {
+		return new HeadersExchange(exchange);
 	}
 
 	@Bean
